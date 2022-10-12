7c650cf852deb0009ca4125ae75dddae4ce747fb
uvc-vivian <viviankim@uvc.co.kr>
2022-10-12 18:04:52 +0900

    separate doosan

--- SshTest/Program.cs
+++ SshTest/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿using System;
 using System.Threading;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
@@ -87,10 +86,6 @@ namespace SshNet
             ThreadStart ts1 = new ThreadStart(DoosanConnect);
             Thread doosan = new Thread(ts1);
             doosan.Start();
-
-            ThreadStart ts2 = new ThreadStart(KukaConnect);
-            Thread kuka = new Thread(ts2);
-            kuka.Start();
         }
 
         static void DoosanConnect()
@@ -105,121 +100,5 @@ namespace SshNet
             AccessSsh doosan = new AccessSsh(doosanName, doosanAddress, doosanPort, doosanUser, doosanPw, doosanPath);
             doosan.ConnectEdge();
         }
-        static void KukaConnect()
-        {
-            //KUKA config
-            string kukaName = "KUKA";
-            string kukaAddress = "10.20.193.101";
-            int kukaPort = 22;
-            string kukaUser = "hyundai";
-            string kukaPw = "opcua5497";
-            string kukaPath = "/home/hyundai/Hyundai-KUKA";
-            AccessSsh kuka = new AccessSsh(kukaName, kukaAddress, kukaPort, kukaUser, kukaPw, kukaPath);
-            kuka.ConnectEdge();
-        }
-        /*
-        //Access SSH & Run SmartConnector (run.sh) 
-        static void AccessSsh(string name, string address, int port, string user, string password, string path)
-        {
-            try
-            {
-                //create new client & session
-                SshClient client = new SshClient(address, port, user, password);
-                client.Connect();
-
-                //create terminal -> used by ShellStream
-                //create a dictionary of terminal modes & add terminal mode
-                IDictionary<TerminalModes, uint> termkvp = new Dictionary<TerminalModes, uint>();
-                termkvp.Add(TerminalModes.ECHO, 53);
-
-                //execute start.sh script
-                ShellStream shellStream = client.CreateShellStream("xterm", 80, 24, 800, 600, 1024, termkvp);
-                var output = shellStream.Expect(new Regex(@"[$>]"));
-                shellStream.WriteLine($"cd {path}");
-                shellStream.WriteLine("sudo sh run.sh");
-                output = shellStream.Expect(new Regex(@"([$#>:])"));
-                shellStream.WriteLine(password);
-
-                string line;
-                while((line = shellStream.ReadLine()) != null)
-                {
-                    Console.WriteLine(line);
-                }
-
-                Console.WriteLine($"{name} SmartConnector Started. Press Z to exit...");
-                while (true)
-                {
-                    if (Console.KeyAvailable)
-                    {
-                        ConsoleKeyInfo key = Console.ReadKey();
-                        if (key.Key == ConsoleKey.Z)
-                        {
-                            shellStream.WriteLine($"cd {path}");
-                            output = shellStream.Expect(new Regex(@"[$>]"));
-                            shellStream.WriteLine("sudo sh stop.sh");
-                            output = shellStream.Expect(new Regex(@"([$#>:])"));
-                            shellStream.WriteLine(password);
-                            Console.WriteLine("\r\n--- SmartConnector Stopped ---");
-                            client.Disconnect();
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                System.Console.WriteLine(ex.ToString());
-            }
-        }
-        static void AccessKUKASsh(string name, string address, int port, string user, string password, string path)
-        {
-            try
-            {
-                //create new client & session
-                SshClient client = new SshClient(address, port, user, password);
-                client.Connect();
-
-                //create terminal -> used by ShellStream
-                //create a dictionary of terminal modes & add terminal mode
-                IDictionary<TerminalModes, uint> termkvp = new Dictionary<TerminalModes, uint>();
-                termkvp.Add(TerminalModes.ECHO, 53);
-
-                //execute start.sh script
-                ShellStream shellStream = client.CreateShellStream("xterm", 80, 24, 800, 600, 1024, termkvp);
-                var output = shellStream.Expect(new Regex(@"[$>]"));
-                shellStream.WriteLine($"cd {path}");
-                shellStream.WriteLine("sudo sh run.sh");
-                output = shellStream.Expect(new Regex(@"([$#>:])"));
-                shellStream.WriteLine(password);
-
-                string line;
-                while ((line = shellStream.ReadLine()) != null)
-                {
-                    Console.WriteLine(line);
-                }
-
-                Console.WriteLine($"{name} SmartConnector Started. Press Z to exit...");
-                while (true)
-                {
-                    if (Console.KeyAvailable)
-                    {
-                        ConsoleKeyInfo key = Console.ReadKey();
-                        if (key.Key == ConsoleKey.Z)
-                        {
-                            shellStream.WriteLine($"cd {path}");
-                            output = shellStream.Expect(new Regex(@"[$>]"));
-                            shellStream.WriteLine("sudo sh stop.sh");
-                            output = shellStream.Expect(new Regex(@"([$#>:])"));
-                            shellStream.WriteLine(password);
-                            Console.WriteLine("\r\n--- SmartConnector Stopped ---");
-                            client.Disconnect();
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                System.Console.WriteLine(ex.ToString());
-            }
-        } */
     }
 }
